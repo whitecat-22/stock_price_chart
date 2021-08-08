ec0f79e9e0f15b559a095814c2c1b86ae76d3cb1
_whitecat_22 <74803154+whitecat-22@users.noreply.github.com>
2021-08-08 11:59:00 +0900

    Amazon ECR (コンテナイメージによるLambdaへのデプロイ) へ対応するため、各種修正

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,6 @@ RUN yum -y install wget
 RUN yum -y install tar
 RUN yum -y install gzip
 
-RUN pip3 install --upgrade pip
-# RUN pip3 install TA-Lib
-# RUN python3 setup.py install
-
-
 # install TA-Lib
 WORKDIR /tmp
 RUN wget --quiet http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz -O ta-lib-0.4.0-src.tar.gz && \
@@ -24,26 +19,24 @@ RUN wget --quiet http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.
     make check && \
     make install && \
     cd .. && \
-    python3 -m pip install --force-reinstall ta-lib && \
+    python3 -m pip install TA-Lib && \
     rm -R ta-lib ta-lib-0.4.0-src.tar.gz
 
-# RUN python3 -m pip install ta-lib --user --global-option='build_ext' --global-option="-I/usr/local/include" --global-option="-L/usr/local/lib"
+RUN export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-# RUN python3 -m pip install --upgrade ta-lib --user --global-option='build_ext' --global-option="-I/usr/local/include" --global-option="-L/etc/ld.so.conf"
+RUN cp -pa /usr/local/lib/libta_lib.so.0* /var/task/
 
+RUN python3 -m pip install --upgrade TA-Lib --user --global-option='build_ext' --global-option="-I/usr/local/include" --global-option="-L/usr/local/lib"
 
-# RUN export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-RUN export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib:
-
-RUN pip3 install numpy
-
-RUN echo "include /usr/local/lib" >> /etc/ld.so.conf
+RUN python3 -m pip install numpy
 
 COPY ./requirements.txt /opt/
-RUN pip3 install -r /opt/requirements.txt -t /var/task
+RUN python3 -m pip install --upgrade pip && \
+    python3 -m pip install -r /opt/requirements.txt -t /var/task
 
-COPY handler.py /var/task/
-# COPY init.py /tmp
-# RUN python3 /tmp/init.py
+WORKDIR /var/task
+COPY handler.py .
+# COPY slack.py .
+# COPY twitter.py .
 
 CMD [ "handler.lambdahandler" ]
--- handler.py
+++ handler.py
@@ -21,8 +21,12 @@ from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
 import talib as ta
 
-from notifiers import slack
-from notifiers import twitter
+#from notifiers import slack
+#from notifiers import twitter
+import tweepy
+from decimal import Decimal, ROUND_HALF_UP
+from slack_sdk.errors import SlackApiError
+from slack_sdk import WebClient
 
 import json
 import logging
@@ -47,6 +51,186 @@ if not os.path.isdir('/tmp'):
     os.mkdir('/tmp')
 
 FILENAME = '%s.csv' % str(today)
+
+
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+dotenv_path = join(dirname(__file__), '.env')
+#load_dotenv(dotenv_path)
+load_dotenv(verbose=True)
+load_dotenv(dotenv_path)
+stock_code = os.environ.get("STOCK_CODE")
+
+"""
+json: Format the data to be sent by the SLack API into JSON
+requests: HTTP client
+"""
+# WebClient insantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+# ID of channel that you want to upload file to
+token = os.environ.get("SLACK_BOT_TOKEN")
+channel_id = os.environ.get("SLACK_CHANNEL_ID")
+
+"""
+json: Format the data to be sent by the Twitter API into JSON
+requests: HTTP client
+"""
+# 各種twitterのKeyをセット CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_KEY_SECRET
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+ACCESS_KEY_SECRET = os.environ.get('ACCESS_KEY_SECRET')
+
+# tweepyの設定
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_KEY_SECRET)
+api = tweepy.API(auth)
+
+
+class Slack():
+    """
+    Notification Class to configure the settings for the Slack API
+    """
+    def __init__(self, date, ohlcv):
+        self.__date = date
+        self.text = self.__format_text(ohlcv)
+
+    @property
+    def date(self):
+        """
+        Property of date to be displayed in Slack text
+        :return: Date
+        """
+        return self.__date
+
+    def __format_text(self, ohlcv):
+        """
+        Create params data for sending Slack notification with API.
+        :param dict[str, str, str, str, str, str] ohlcv:
+        :type ohlcv: {
+            'Date': '2020-12-29',
+            'Open': '7620',
+            'High': '8070',
+			'Low': '7610',
+			'Close': '8060',
+			'Volume': '823700'
+        }
+        :return: String
+        """
+        open_ = Decimal(str(ohlcv['Open'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+        high_ = Decimal(str(ohlcv['High'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+        low_ = Decimal(str(ohlcv['Low'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+        close_ = Decimal(str(ohlcv['Close'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+
+        text = f"本日は{self.date.strftime('%Y年%m月%d日')}です。\n" \
+               f"取得可能な最新日付の株価情報をお知らせします。 \n\n"\
+               f"*銘柄* {str(stock_code)}\n" \
+               f"*日付* {str(ohlcv['Date'])}\n" \
+               f"*始値* {float(open_)}\n" \
+               f"*高値* {float(high_)}\n" \
+            f"*安値* {float(low_)}\n" \
+            f"*終値* {float(close_)}\n" \
+            f"*出来高* {float(ohlcv['Volume'])}"
+   #f"*出来高* {int(ohlcv['Volume']):,d}"
+        return text
+
+    def post(self):
+        """
+        POST request to Slack file upload API
+        API docs: https://slack.com/api/files.upload
+        """
+        # The name of the file you're going to upload
+        file = open(f"/tmp/{str(self.date)}.png", 'rb')
+        title = f"{str(self.date)}.png"
+        # Call the files.upload method using the WebClient
+        # Uploading files requires the `files:write` scope
+        try:
+            client.files_upload(
+                channels=channel_id,
+                initial_comment=self.text,
+                file=file,
+                title=title
+            )
+        except Exception as e:
+            print(e)
+
+
+class Twitter():
+    """
+    Notification Class to configure the settings for the Twitter API
+    """
+    def __init__(self, date, ohlcv):
+        self.__date = date
+        self.text = self.__format_text(ohlcv)
+
+    @property
+    def date(self):
+        """
+        Property of date to be displayed in Slack text
+        :return: Date
+        """
+        return self.__date
+
+    def __format_text(self, ohlcv):
+        """
+        Create params data for sending Twitter notification with API.
+        :param dict[str, str, str, str, str, str] ohlcv:
+        :type ohlcv: {
+            'Date': '2020-12-29',
+            'Open': '7620',
+            'High': '8070',
+			'Low': '7610',
+			'Close': '8060',
+			'Volume': '823700'
+        }
+        :return: String
+        """
+        open_ = Decimal(str(ohlcv['Open'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+        high_ = Decimal(str(ohlcv['High'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+        low_ = Decimal(str(ohlcv['Low'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+        close_ = Decimal(str(ohlcv['Close'])).quantize(
+            Decimal('0.01'), rounding=ROUND_HALF_UP)
+
+        text = f"本日は{self.date.strftime('%Y年%m月%d日')}です。\n" \
+               f"取得可能な最新日付の株価情報をお知らせします。 \n\n"\
+               f"銘柄  {str(stock_code)}\n" \
+               f"日付  {str(ohlcv['Date'])}\n" \
+               f"始値  {float(open_)}\n" \
+               f"高値  {float(high_)}\n" \
+               f"安値  {float(low_)}\n" \
+               f"終値  {float(close_)}\n" \
+            f"出来高  {float(ohlcv['Volume'])}"
+   #f"出来高  {int(ohlcv['Volume']):,d}"
+        return text
+
+    def post(self):
+        """
+        POST request to Twitter API
+        API docs: https://developer.twitter.com/en/docs/twitter-api/api-reference-index
+        """
+        # The name of the file you're going to upload
+        file = open(f"/tmp/{str(self.date)}.png", 'rb')
+        title = f"{str(self.date)}.png"
+        # Call the files.upload method using the WebClient
+        # Uploading files requires the `files:write` scope
+        try:
+            file_names = ['/tmp/' + title, ]
+            media_ids = []
+            for filename in file_names:
+                res = api.media_upload(filename)
+                media_ids.append(res.media_id)
+            # tweet with multiple images
+            api.update_status(status=self.text + "\n\n" +
+                              title, media_ids=media_ids)
+        except Exception as e:
+            print(e)
 
 
 def generate_stock_chart_image():
@@ -238,8 +422,8 @@ def lambdahandler(event, context):
         for i, row in enumerate(csv.DictReader(file, header)):
             # Send only the most recent data to Slack notification
             if i == 0:
-                slack.Slack(today, row).post()
-                twitter.Twitter(today, row).post()
+                Slack(today, row).post()
+                Twitter(today, row).post()
 
     return {
         'statusCode': 200,
